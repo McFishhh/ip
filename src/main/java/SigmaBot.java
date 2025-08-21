17e3205c2f06604e8d684cba44fb691a93457006
McFishhh <shawnweeweewee@gmail.com>
2025-08-22 01:31:18 +0800

    feat: added user input and bot echoing

--- src/main/java/SigmaBot.java
+++ src/main/java/SigmaBot.java
@@ -1,14 +1,32 @@
+import java.util.Scanner;
+
 public class SigmaBot {
-    public static void main(String[] args) {
-        String greeting = " Hello! I'm SigmaBot\r\n" + //
-                        " What can I do for you?\r\n";
-                    
-        String goodbye = " Bye. Hope to see you again soon!\r\n";
+
+    public SigmaBot() { 
+    }
+
+    public static String echo(Scanner scanner) {
+        String msg = scanner.nextLine();
         
-        System.out.println("____________________________________________________________\r\n" + //
-                         greeting + //
-                        "____________________________________________________________\r\n" + //
-                         goodbye + //
-                        "____________________________________________________________");
+        return msg;
+    }
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String greeting = "\tHello! I'm SigmaBot\r\n" + "\tWhat can I do for you?\r\n";
+        String goodbye = "\tBye. Hope to see you again soon!\r\n";
+        String sep = "\t____________________________________________________________\r\n";
+        
+        System.out.println(sep + greeting + sep);
+    
+        String msg = echo(scanner);
+        while (!msg.toLowerCase().equals("bye")) {
+            System.out.println(sep + "\t" + msg + "\r\n" + sep);
+            msg = echo(scanner);
+        } 
+
+        System.out.println(sep + goodbye + sep);
     }
 }
