cabbd4c4833257b5d18eeeee76704e60c3feb58e
McFishhh <shawnweeweewee@gmail.com>
2025-08-22 01:10:17 +0800

    edit: renamed bot and changed greeting message

---
+++ src/main/java/SigmaBot.java
@@ -0,0 +1,14 @@
+public class SigmaBot {
+    public static void main(String[] args) {
+        String greeting = " Hello! I'm SigmaBot\r\n" + //
+                        " What can I do for you?\r\n";
+                    
+        String goodbye = " Bye. Hope to see you again soon!\r\n";
+        
+        System.out.println("____________________________________________________________\r\n" + //
+                         greeting + //
+                        "____________________________________________________________\r\n" + //
+                         goodbye + //
+                        "____________________________________________________________");
+    }
+}
