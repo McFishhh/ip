b44f6fa5179146a34d0945e8a0129b34b6b2b6a9
McFishhh <shawnweeweewee@gmail.com>
2025-08-27 23:24:07 +0800

    add ability to save and load files

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -9,6 +9,27 @@ public class DeadlineTask extends TodoTask {
         this.deadline = deadline;
     }
 
+    public DeadlineTask(String description, boolean isDone, String deadline) throws SigmaBotException{
+        super(description, isDone);
+        if (deadline.equals("")) {
+            throw new SigmaBotException("Hey, theres no deadline!");
+        }
+        this.deadline = deadline;
+    }
+
+    public String getTaskIcon() {
+        return "D";
+    }
+
+    public String encodeSaveFormat() {
+        return this.getTaskIcon() + "," + this.isDone + "," + this.description + "," + this.deadline;
+    }
+
+    public static DeadlineTask decodeSaveFormat(String encoded) {
+        String[] encodedSplit = encoded.split(",");
+        return new DeadlineTask(encodedSplit[2], Boolean.parseBoolean(encodedSplit[1]), encodedSplit[3]);
+    }
+
     @Override
     public String toString() {
         return "[D]" + "[" + this.getStatusIcon() + "] " + this.description + //
